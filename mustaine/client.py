17fb03fde3b5904c35d4b75f93eaf956b7271a82
Brandon Gilmore <brandon@mg2.org>
2010-05-10 23:06:15 -0700

    oops =)

--- mustaine/client.py
+++ mustaine/client.py
@@ -77,19 +77,6 @@ class HessianProxy(object):
             self._client.putheader(*header)
         
         request = encode_object(HessianCall(method, args))
-
-        def hexdump(src, length=8):
-            result = []
-            digits = 4 if isinstance(src, unicode) else 2
-            for i in xrange(0, len(src), length):
-               s = src[i:i+length]
-               hexa = b' '.join(["%0*X" % (digits, ord(x))  for x in s])
-               text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.'  for x in s])
-               result.append( b"%04X   %-*s   %s" % (i, length*(digits + 1), hexa, text) )
-            return b'\n'.join(result)
-        print hexdump(request)
-
-
         self._client.putheader("Content-Length", str(len(request)))
         self._client.endheaders()
         self._client.send(str(request))
