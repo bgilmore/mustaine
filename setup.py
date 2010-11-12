77ee41cf312f6bd23e005f420e9f78496c1b251f
Brandon Gilmore <brandon@mg2.org>
2010-11-11 19:48:02 -0800

    add setuptools scaffolding for speedups

--- setup.py
+++ setup.py
@@ -1,29 +1,44 @@
 import os, sys
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Extension, Feature
 from mustaine import __version__
 
 if sys.version_info < (2,6):
     raise NotImplementedError("mustaine requires Python 2.6 or later")
 
+src_root = os.path.dirname(__file__)
+
+speedups = Feature(
+    description = "C extension providing speed improvements",
+    standard = True,
+    optional = True,
+
+    ext_modules = [
+        Extension(
+            "mustaine._speedups",
+            depends = [os.path.join(src_root, "ext", "speedups.h")],
+            sources = [os.path.join(src_root, "ext", "speedups.c")],
+        )
+    ]
+)
+
 setup(
     name = "mustaine",
     version = __version__,
     description = "Hessian RPC Library",
-    long_description = file(
-        os.path.join(
-            os.path.dirname(__file__),
-            'README.rst'
-        )
-    ).read(),
+    long_description = file(os.path.join(src_root, "README.rst")).read(),
+
+    features = {
+        "speedups": speedups,
+    },
 
     classifiers = [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Object Brokering',
-        'Topic :: Software Development :: Libraries',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Object Brokering",
+        "Topic :: Software Development :: Libraries",
     ],
 
     url = "http://github.com/bgilmore/mustaine",
