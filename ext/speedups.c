b4f3ffe306b6cd0f55f9cf57a882d1762b880a9b
Brandon Gilmore <brandon@mg2.org>
2010-11-14 22:59:17 -0800

    fix edge case coverage

--- ext/speedups.c
+++ ext/speedups.c
@@ -5,29 +5,35 @@
  
 #include "Python.h"
 
+static PyObject *PyUnicode_NIL;
+
+
+/**
+ * _speedups.read_string(<obj:stream>, <int:count>)
+ */
+
 PyDoc_STRVAR(py_read_string_docstr, "deserializes a unicode string from a Hessian input stream");
 PyObject * py_read_string(PyObject *self, PyObject *args)
 {
 	PyObject *stream, *count;   /* arguments */
-	PyObject *shim, *result;    /* safe refs */
 	PyObject *read   = NULL,
 	         *buffer = NULL,
 	         *excess = NULL,
 	         *chunk  = NULL,
-	         *chunks = NULL;
-	Py_ssize_t consumed;
-	long remaining, needed;
+	         *chunks = NULL,
+	         *result = NULL;
+	Py_ssize_t consumed, remaining;
 
 	if (! PyArg_UnpackTuple(args, "read_string", 2, 2, &stream, &count))
 		return NULL;
 
-	remaining = PyInt_AsLong(count);
+	remaining = (long) PyInt_AsLong(count);
 	if ((remaining == -1) && (PyErr_Occurred()))
 		return NULL;
 
 	/* trap door */
 	if (remaining <= 0)
-		return Py_BuildValue("u", "");
+		return PyUnicode_NIL;
 
 	read = PyObject_GetAttrString(stream, "read");
 	if (! PyCallable_Check(read))
@@ -38,19 +44,14 @@ PyObject * py_read_string(PyObject *self, PyObject *args)
 		goto err;
 
 	while (remaining > 0) {
-		if (excess != NULL)
-			needed = remaining - PyString_GET_SIZE(excess);
-		else
-			needed = remaining;
-
 		/* read minimum viable chunk */
-		buffer = PyObject_CallFunction(read, "i", needed);
-		if (PyString_Size(buffer) != needed) {
+		buffer = PyObject_CallFunction(read, "i", remaining);
+		if (PyString_Size(buffer) != remaining) {
 			PyErr_SetString(PyExc_EOFError, "encountered unexpected end of stream");
 			goto err;
 		}
 
-		/* prepend overflow bytes from previous rounds if necessary */
+		/* pull in excess bytes from the last iteration */
 		if (excess != NULL) {
 			PyString_ConcatAndDel(&excess, buffer);
 			if (excess == NULL)
@@ -60,41 +61,40 @@ PyObject * py_read_string(PyObject *self, PyObject *args)
 			excess = NULL;
 		}
 
-		/* decode the buffer and store the resulting chunk */
-		chunk = PyUnicode_DecodeUTF8Stateful(PyString_AS_STRING(buffer), remaining, "strict", &consumed);
+		/* decode the buffer and store the result chunk */
+		chunk = PyUnicode_DecodeUTF8Stateful(PyString_AS_STRING(buffer), PyString_GET_SIZE(buffer), "strict", &consumed);
 		if (chunk == NULL)
 			goto err;
 
 		if (PyList_Append(chunks, chunk) != 0)
 			goto err;
 
-		/* check if we need to stash leftover bytes for the next round */
+		/* stash any remaining undecoded bytes for next iteration */
 		if (consumed < PyString_GET_SIZE(buffer)) {
 			excess = PyString_FromStringAndSize(PyString_AS_STRING(buffer) + consumed, PyString_GET_SIZE(buffer) - consumed);
+			if (excess == NULL)
+				goto err;
 		}
 
 		remaining -= PyUnicode_GET_SIZE(chunk);
-
-		Py_DECREF(chunk);
 		Py_DECREF(buffer);
+		Py_DECREF(chunk);
 	}
-
 
 	Py_DECREF(read);
 
-	shim   = PyUnicode_FromWideChar((const wchar_t *) "", 0);
-	result = PyUnicode_Join(shim, chunks);
+	result = PyUnicode_Join(PyUnicode_NIL, chunks);
+	if (result == NULL)
+		goto err;
 
-	Py_DECREF(shim);
 	Py_DECREF(chunks);
 
 	return result;
 
 err:
 	Py_XDECREF(read);
-	Py_XDECREF(buffer);
+//	Py_XDECREF(buffer); /* unsafe to collect? WHY? */
 	Py_XDECREF(excess);
-	Py_XDECREF(chunk);
 	Py_XDECREF(chunks);
 	return NULL;
 }
@@ -112,6 +112,10 @@ static PyMethodDef speedups_methods[] = {
 PyDoc_STRVAR(module_doc, "native codec implementations");
 PyMODINIT_FUNC init_speedups(void)
 {
+	PyUnicode_NIL = PyUnicode_FromWideChar((wchar_t *) "", 0);
+	if (PyUnicode_NIL == NULL)
+		return;
+
 	Py_InitModule3("_speedups", speedups_methods, module_doc);
 }
 
